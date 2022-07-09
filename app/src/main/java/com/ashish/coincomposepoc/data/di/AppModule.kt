d04a91700bc2e57497dfd0ba3dc6da254464e8eb
Ashish <ashish.p@phynart.com>
2022-07-09 10:16:56 +0530

    Add hilt module, viewmodel and data state

---
+++ app/src/main/java/com/ashish/coincomposepoc/data/di/AppModule.kt
@@ -0,0 +1,34 @@
+package com.ashish.coincomposepoc.data.di
+
+import com.ashish.coincomposepoc.common.Constants
+import com.ashish.coincomposepoc.data.remote.CoinApi
+import com.ashish.coincomposepoc.data.repository.CoinRepositoryImpl
+import com.ashish.coincomposepoc.domain.repository.CoinRepository
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun providesCoinApi(): CoinApi {
+        return Retrofit.Builder()
+            .baseUrl(Constants.BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(CoinApi::class.java)
+    }
+
+    @Provides
+    @Singleton
+    fun provideCoinRepository(api: CoinApi): CoinRepository {
+        return CoinRepositoryImpl(api)
+    }
+}
---
+++ app/src/main/java/com/ashish/coincomposepoc/presentation/coin_detail/CoinDetailState.kt
@@ -0,0 +1,9 @@
+package com.ashish.coincomposepoc.presentation.coin_detail
+
+import com.ashish.coincomposepoc.domain.model.CoinDetail
+
+data class CoinDetailState(
+    val isLoading: Boolean = false,
+    val coins: CoinDetail? = null,
+    val error: String = ""
+)
